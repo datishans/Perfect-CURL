02828e95caa39686a35a27871b486d008d571a44
Hans Bijkerk <datishans@users.noreply.github.com>
2018-11-15 16:50:07 +0100

    Update CURLRequest+resolve.swift

--- Sources/CURLRequest+resolve.swift
+++ Sources/CURLRequest+resolve.swift
@@ -1,12 +1,15 @@
 import cURL
 
 public extension CURLRequest {
-    class func resolve(host: String, port: Int) {
-        let curl = CURL()
-        curl.setOption(CURLOPT_RESOLVE, s: host + ":\(port):127.0.0.1")
-    }
+//    class func resolve(host: String, port: Int) {
+//        let curl = CURL()
+//        curl.setOption(CURLOPT_RESOLVE, s: host + ":\(port):127.0.0.1")
+//    }
     func resolve(host: String, port: Int) -> CURLRequest {
-        curl.setOption(CURLOPT_RESOLVE, s: host + ":\(port):127.0.0.1")
+        let s = host + ":\(port):127.0.0.1"
+        print(s)
+        curl.setOption(CURLOPT_RESOLVE, s: s)
+
         return self
     }
 }
